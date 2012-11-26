7d64767f0047ebab72361685a2bb9d6e12dedf6a
Tully <tully@apiary.io>
2012-11-26 17:53:55 +0100

    better exceptions logging

--- sentry-transport.js
+++ sentry-transport.js
@@ -1,39 +1,39 @@
 var util = require('util'),
     raven = require('raven'),
     winston = require('winston')
-    _ = require('underscore');
-    
+_ = require('underscore');
+
 var Sentry = winston.transports.CustomerLogger = function (options) {
 
-  this.name = 'Sentry';
-  this._dsn = options.dsn || '';
-  this._patchGlobal = options.patchGlobal || false;
-  this._sentry = new raven.Client(this._dsn);
-  this._logger = options.logger || 'root';
-  
-  if(this.patchGlobal) {
-    this._sentry.patchGlobal();
-  }
-  
-  this._levels_map = {
-    silly: 'debug',
-    verbose: 'debug',
-    info: 'info',
-    debug: 'debug',
-    warn: 'warning',
-    error: 'error',
-  }
+    this.name = 'Sentry';
+    this._dsn = options.dsn || '';
+    this._patchGlobal = options.patchGlobal || false;
+    this._sentry = new raven.Client(this._dsn);
+    this._logger = options.logger || 'root';
 
-  // Set the level from your options
-  this.level = options.level || 'info';
+    if(this.patchGlobal) {
+        this._sentry.patchGlobal();
+    }
 
-  // Handle errors
-  this._sentry.on('error', function() {
-    console.log("Cannot talk to sentry!");
-  });
-  
-  // Expose sentry client to winston.Logger
-  winston.Logger.prototype.sentry_client = this._sentry;
+    this._levels_map = {
+        silly: 'debug',
+        verbose: 'debug',
+        info: 'info',
+        debug: 'debug',
+        warn: 'warning',
+        error: 'error',
+    }
+
+    // Set the level from your options
+    this.level = options.level || 'info';
+
+    // Handle errors
+    this._sentry.on('error', function() {
+        console.log("Cannot talk to sentry!");
+    });
+
+    // Expose sentry client to winston.Logger
+    winston.Logger.prototype.sentry_client = this._sentry;
 };
 
 //
@@ -61,20 +61,20 @@ Sentry.prototype.log = function (level, msg, meta, callback) {
     }
 
     try {
-        if((meta instanceof Error) && (stack = meta.stack)) {
+        if(meta  && (stack = meta.stack)) {
             // Support exceptions logging
             if(meta.message) {
                 msg += " | " + meta.message;
             }
             e = new Error(msg);
-            e.stack = meta.stack;
+            e.stack = stack;
             e.name = level
             this._sentry.captureError(e, extra, function(err) {
                 callback(null, true);
             });
-        } else if ((msg instanceof Error) && (stack = msg.stack)) {
+        } else if (msg && (stack = msg.stack)) {
             e = new Error(msg.message)
-            e.stack = msg.stack;
+            e.stack = stack;
             e.name = level
             this._sentry.captureError(e, extra, function(err) {
                 callback(null, true);
